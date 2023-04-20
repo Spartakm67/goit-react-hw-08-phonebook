5deb02b3a93404461a1d03323a065be3d63b1986
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-04-20 19:53:53 +0300

    Store Updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,49 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactReducer } from './contactsSlice';
+// import { filterReducer } from './filterSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactReducer,
+//     filter: filterReducer,
+//   },
+// });
+
 import { configureStore } from '@reduxjs/toolkit';
 import { contactReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+import { combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+export const rootReducer = combineReducers({
+  contacts: contactReducer,
+  filter: filterReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactReducer,
-    filter: filterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store);
